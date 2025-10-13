10119fc426dc7318ea72534bc45a377ffa7af727
discosultan <jaanusvarus@gmail.com>
2025-10-13 14:43:59 +0200

    refactor(serde-ext): use display instead of tostring impl in serialize helpers

--- serde-ext/src/serialize.rs
+++ serde-ext/src/serialize.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use serde::{
     Serialize, Serializer,
     ser::{SerializeSeq, SerializeTuple},
@@ -17,37 +19,49 @@ pub fn const_generic_array<S: Serializer, T: Serialize, const N: usize>(
     s.end()
 }
 
-/// Custom serialization function that uses [`ToString`].
-pub fn to_string<S>(value: &impl ToString, serializer: S) -> Result<S::Ok, S::Error>
+/// Custom serialization function that uses [`Display`].
+pub fn to_string<S>(value: &impl Display, serializer: S) -> Result<S::Ok, S::Error>
 where
     S: Serializer,
 {
-    serializer.serialize_str(&value.to_string())
+    serializer.collect_str(value)
 }
 
-/// Custom serialization function that uses [`ToString`].
-pub fn to_string_opt<S>(value: &Option<impl ToString>, serializer: S) -> Result<S::Ok, S::Error>
+/// Custom serialization function that uses [`Display`].
+pub fn to_string_opt<S>(value: &Option<impl Display>, serializer: S) -> Result<S::Ok, S::Error>
 where
     S: Serializer,
 {
     if let Some(value) = value {
-        serializer.serialize_str(&value.to_string())
+        serializer.collect_str(&value)
     } else {
         serializer.serialize_none()
     }
 }
 
-/// Custom serialization function that uses [`ToString`].
+/// Custom serialization function that uses [`Display`].
 pub fn slice_elements_to_string<S>(
-    values: &[impl ToString],
+    values: &[impl Display],
     serializer: S,
 ) -> Result<S::Ok, S::Error>
 where
     S: Serializer,
 {
+    /// Wrapper that serializes using [`Display`].
+    struct DisplayWrapper<'a, T: Display>(&'a T);
+
+    impl<'a, T: Display> Serialize for DisplayWrapper<'a, T> {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            serializer.collect_str(self.0)
+        }
+    }
+
     let mut seq = serializer.serialize_seq(Some(values.len()))?;
     for value in values {
-        seq.serialize_element(&value.to_string())?;
+        seq.serialize_element(&DisplayWrapper(value))?;
     }
     seq.end()
 }
