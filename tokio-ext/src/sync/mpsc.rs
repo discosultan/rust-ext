34947da8ebaca1fd8dcc8df75be8c70ae302090f
discosultan <jaanusvarus@gmail.com>
2025-10-13 17:12:56 +0200

    fix(tokio-ext): make mpsc try send sync

--- tokio-ext/src/sync/mpsc.rs
+++ tokio-ext/src/sync/mpsc.rs
@@ -9,10 +9,7 @@ use tracing::warn;
 pub trait SenderExt<'a, T: 'a> {
     fn send_log_backpressure(&self, value: T) -> impl Future<Output = Result<(), SendError<T>>>;
 
-    fn try_send_discard_full_log_backpressure(
-        &self,
-        value: T,
-    ) -> impl Future<Output = Result<(), SendError<T>>>;
+    fn try_send_discard_full_log_backpressure(&self, value: T) -> Result<(), SendError<T>>;
 
     fn reserve_many_log_backpressure(
         &'a self,
@@ -28,12 +25,24 @@ impl<'a, T: 'a> SenderExt<'a, T> for Sender<T> {
     }
 
     #[track_caller]
-    fn try_send_discard_full_log_backpressure(
-        &self,
-        value: T,
-    ) -> impl Future<Output = Result<(), SendError<T>>> {
-        let caller = Location::caller();
-        try_send_log_backpressure_impl(self, value, caller)
+    fn try_send_discard_full_log_backpressure(&self, value: T) -> Result<(), SendError<T>> {
+        let capacity = self.max_capacity();
+        let len = capacity - self.capacity();
+        if len > capacity / 2 {
+            warn!(
+                len = len,
+                capacity = capacity,
+                "High channel usage in mpsc try send."
+            );
+        }
+        match self.try_send(value) {
+            Ok(()) => Ok(()),
+            Err(TrySendError::Full(_)) => {
+                warn!("Discarded value in mpsc try send.");
+                Ok(())
+            }
+            Err(TrySendError::Closed(value)) => Err(SendError(value)),
+        }
     }
 
     #[track_caller]
@@ -63,36 +72,6 @@ async fn send_log_backpressure_impl<T>(
         );
     }
     sender.send(value).await
-}
-
-async fn try_send_log_backpressure_impl<T>(
-    sender: &Sender<T>,
-    value: T,
-    caller: &'static Location<'static>,
-) -> Result<(), SendError<T>> {
-    let capacity = sender.max_capacity();
-    let len = capacity - sender.capacity();
-    if len > capacity / 2 {
-        warn!(
-            len = len,
-            capacity = capacity,
-            file = caller.file(),
-            line = caller.line(),
-            "High channel usage in mpsc try send."
-        );
-    }
-    match sender.try_send(value) {
-        Ok(()) => Ok(()),
-        Err(TrySendError::Full(_)) => {
-            warn!(
-                file = caller.file(),
-                line = caller.line(),
-                "Discarded value in mpsc try send."
-            );
-            Ok(())
-        }
-        Err(TrySendError::Closed(value)) => Err(SendError(value)),
-    }
 }
 
 async fn reserve_many_log_backpressure_impl<'a, T: 'a>(
